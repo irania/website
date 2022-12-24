873db50a69350a3b4f27aba23f76ff2d37af76c3
Irania <atefeh.ir72@gmail.com>
2022-12-24 19:29:38 +0330

    Initial commit.

---
+++ src/config.ts
@@ -0,0 +1,5 @@
+// Place any global data in this file.
+// You can import this data from anywhere in your site by using the `import` keyword.
+
+export const SITE_TITLE = 'Atefeh Irani Personal Website';
+export const SITE_DESCRIPTION = 'Atefeh Irani Personal Website';
